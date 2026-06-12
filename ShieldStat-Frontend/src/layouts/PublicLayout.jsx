1b7ea566a267dcafd889c85ad9c79801d9d70bac
AshishBharti783 <71437783+AshishBharti783@users.noreply.github.com>
2026-06-12 19:39:19 +0530

    Delete ShieldStat-Frontend directory

--- ShieldStat-Frontend/src/layouts/PublicLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-function PublicLayout({ isDarkMode, onToggleDarkMode }) {
-  return (
-    <Outlet context={{ isDarkMode, onToggleDarkMode }} />
-  );
-}
-
-export default PublicLayout;
